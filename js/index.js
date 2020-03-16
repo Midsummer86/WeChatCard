4bfef475363d7da4c67542d1b3886650c41fda75
liwenzhong <liwenzhong@pxjy.com>
2020-03-16 17:40:27 +0800

    tow

--- js/index.js
+++ js/index.js
@@ -3,25 +3,22 @@ var alert = document.querySelector(".alert")
 var footer = document.querySelector(".footer")
 var mask = document.querySelector(".mask")
 var body = document.querySelector("body")
-var input = document.getElementById("input")
+var e = document.getElementById("copy")
 btn.onclick = function(){
-    input.style.display='block'
     alert.style.display='block'
     mask.style.display='block'
     body.style.position='fixed'
     alert.classList.add('donghua')
-    input.select() // 选中文本
+    e.select() // 选中文本
     document.execCommand("copy")
 }
 footer.onclick = function(){
-    input.style.display='none'
     alert.style.display='none'
     mask.style.display='none'
     window.location.href="weixin://";
     body.style.position='static'
 }
 mask.onclick = function(){
-    input.style.display='none'
     alert.style.display='none'
     mask.style.display='none'
     body.style.position='static'
@@ -36,15 +33,3 @@ function touchs(e) {
 }
 footer.addEventListener('touchstart',touchs,false);
 footer.addEventListener('touchend',touchs,false);
-
-// 屏幕固定
-// function toggleBody(isPin){
-//     if(isPin){
-//         document.body.style.height = '100vh'
-//         document.body.style['overflow-y'] = 'hidden'
-//     }
-//     else{
-//         document.body.style.height = 'unset'
-//         document.body.style['overflow-y'] = 'auto'
-//     }
-// }
